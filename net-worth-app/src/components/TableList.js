de72b8f15c955e7ece50f0df1d7ab35a2c63e893
Michelle Naylor <michnaylor@gmail.com>
2019-09-28 14:17:28 -0600

    added dummy liabilities table

--- net-worth-app/src/components/TableList.js
+++ net-worth-app/src/components/TableList.js
@@ -10,7 +10,15 @@ class TableList extends Component {
                 name: "Some asset",
                 category: "Savings",
                 is_asset: true,
-                amount: 1
+                amount: 1,
+                id: 0
+            }],
+            liabilities: [{
+                name: "Some liability",
+                category: "mortgage",
+                is_asset: false,
+                amount: 1,
+                id: 1
             }],
             searchString: ''
         }
@@ -28,6 +36,9 @@ class TableList extends Component {
                     <Grid container spacing={10} style={{padding: 24}}>
                         <Grid item xs={12} sm={6} lg={4} xl={3}>
                             <Table entries={this.state.assets} is_asset={true} />
+                        </Grid>
+                        <Grid item xs={12} sm={6} lg={4} xl={3}>
+                            <Table entries={this.state.liabilities} is_asset={false} />
                         </Grid>
                     </Grid>
                 </div>
