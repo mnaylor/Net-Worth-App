357faf8fdfc4e7b5d2e33c32eecc331f536ec88d
Michelle Naylor <michnaylor@gmail.com>
2019-09-30 19:51:12 -0600

    Linted navbar

--- net-worth-app/src/components/NavBar.js
+++ net-worth-app/src/components/NavBar.js
@@ -3,24 +3,16 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
-class NavBar extends Component {
-  constructor() {
-      super();
-  }
-
-  render() {
+function NavBar () {
     return (
-    <div>
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6">
-                Net Worth Calculator
-                </Typography>
-            </Toolbar>
-        </AppBar>
-    </div>
+      <AppBar position="static">
+          <Toolbar>
+              <Typography variant="h6">
+                  Net Worth Calculator
+              </Typography>
+          </Toolbar>
+    </AppBar>
     )
-  }
 }
 
 export default NavBar;
