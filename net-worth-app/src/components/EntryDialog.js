198c7cae252e457fc5f75fddb617e51331d320af
Michelle Naylor <michnaylor@gmail.com>
2019-09-30 19:45:29 -0600

    linted EntryDialog

--- net-worth-app/src/components/EntryDialog.js
+++ net-worth-app/src/components/EntryDialog.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import AddBoxIcon from '@material-ui/icons/AddBox';
 import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import {Dialog, DialogActions, DialogContent, DialogTitle} from '@material-ui/core/';
 import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import Tooltip from '@material-ui/core/Tooltip';
@@ -78,33 +71,32 @@ class EntryDialog extends Component {
                 <DialogTitle id="form-dialog-title">{this.props.title}</DialogTitle>
                 <DialogContent>
                     <TextField autoFocus margin="dense" name="category"
-                            type="string" value={this.props.data ? this.props.data.category : this.state.data.category}
-                            onChange={this.handleInputChange}
-                            label={this.state.isValid.category ? "Category": "Less than 50 characters"}
-                            InputProps={{ error: !this.state.isValid.category }}
+                        type="string" value={this.props.data ? this.props.data.category : this.state.data.category}
+                        onChange={this.handleInputChange}
+                        label={this.state.isValid.category ? "Category": "Less than 50 characters"}
+                        InputProps={{ error: !this.state.isValid.category }}
                     />
                 </DialogContent>
                 <DialogContent>
                     <TextField margin="dense" name="name"
-                            type="string" value={this.props.data ? this.props.data.name : this.state.data.name}
-                            onChange={this.handleInputChange}
-                            label={this.state.isValid.name ? "Name": "Less than 50 characters"}
-                            InputProps={{ error: !this.state.isValid.name }}
+                        type="string" value={this.props.data ? this.props.data.name : this.state.data.name}
+                        onChange={this.handleInputChange}
+                        label={this.state.isValid.name ? "Name": "Less than 50 characters"}
+                        InputProps={{ error: !this.state.isValid.name }}
                     />
                 </DialogContent>
                 <DialogContent>
                     <TextField margin="dense" name="amount" type="number"
-                            label={this.state.isValid.amount ? "Amount": "Greater than 0"}
-                            InputProps={{ inputProps: {min: 0, max: Number.MAX_SAFE_INTEGER}, error: !this.state.isValid.amount,
-                                startAdornment: <InputAdornment position="start">USD$</InputAdornment>
-                    }}
-                    value={this.props.data ? this.props.data.amount : this.state.data.amount}
-                    onChange={this.handleInputChange}
+                        label={this.state.isValid.amount ? "Amount": "Greater than 0"}
+                        InputProps={{ inputProps: {min: 0, max: Number.MAX_VALUE}, error: !this.state.isValid.amount,
+                            startAdornment: <InputAdornment position="start">USD$</InputAdornment>}}
+                        value={this.props.data ? this.props.data.amount : this.state.data.amount}
+                        onChange={this.handleInputChange}
                     />
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={this.props.closeDialog} color="primary">
-                    Cancel
+                        Cancel
                     </Button>
                     <Tooltip title="Category, Name, and Amount must have a value" aria-label="add">
                         <span>
